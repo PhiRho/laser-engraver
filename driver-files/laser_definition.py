4a39020019192e61abfba622386f71543417a6d2
Pippa H <pippa+github@pippah.net>
2025-03-12 13:43:04 +0200

    Pull out a couple of the functions for testability
    Tests highlight misunderstandings, particularly around functions like
    `sleep` which take an argument in seconds not milliseconds.

--- driver-files/laser_definition.py
+++ driver-files/laser_definition.py
@@ -1,7 +1,9 @@
+import logging
 import pigpio
 import time
 
 class LaserDefinition:
+    logger = logging.getLogger(__name__)
     """
     A class to control a laser cutter's motors and laser module. This assumes NEMA 17 stepper motors.
 
@@ -34,6 +36,8 @@ class LaserDefinition:
         pass
 
 class Motor:
+    LOGGER = logging.getLogger(__name__)
+
     MICROSTEP_MATRIX = {
         1: (0, 0, 0),
         2: (1, 0, 0),
@@ -64,6 +68,8 @@ class Motor:
         self.ms3 = ms3
         self.pi = pi
         self.enable_pins()
+        self.LOGGER.info("Motor initialized with pins: Step: %s, Direction: %s, MS1: %s, MS2: %s, MS3: %s", self.step, self.direction, self.ms1, self.ms2, self.ms3)
+        self.set_microstep(1)
 
     def enable_pins(self):
         self.pi.set_mode(self.step, pigpio.OUTPUT)
@@ -71,6 +77,12 @@ class Motor:
         self.pi.set_mode(self.ms1, pigpio.OUTPUT)
         self.pi.set_mode(self.ms2, pigpio.OUTPUT)
         self.pi.set_mode(self.ms3, pigpio.OUTPUT)
+
+    def set_microstep(self, microstep):
+        self.pi.write(self.ms1, self.MICROSTEP_MATRIX[microstep][0])
+        self.pi.write(self.ms2, self.MICROSTEP_MATRIX[microstep][1])
+        self.pi.write(self.ms3, self.MICROSTEP_MATRIX[microstep][2])
+        self.LOGGER.info("Microstep set to: %s", microstep)
 
     """
     Attributes: 
@@ -80,14 +92,30 @@ class Motor:
     def move(self, distance, speed):
         full_revolution = self.TEETH_PER_REVOLUTION * self.TOOTH_PITCH
         # Distance is a number of steps
-        step_count = int(distance / full_revolution) * self.STEPS_PER_REVOLUTION
-        # Speed is a number of steps per second
-        steps_per_second = (speed / full_revolution) * self.STEPS_PER_REVOLUTION
-        step_delay = (1 / steps_per_second) / 1000000.0 # convert to microseconds
+        step_count = self.step_count_from_distance(distance)
+        # Speed is a number of steps per second, but we need to know the number of millis between steps
+        step_delay = self.step_delay_from_speed(speed)
 
-        if (step_delay > 100):
+        # Moving too fast or too slow is going to cause problems with the smoothness of the movement, and 
+        # will do interesting things to precision.
+        if (step_delay > (100 / 1000.0)):
             raise Exception("Step delay is too long: ", step_delay)
+        elif (step_delay < (1 / 1000.0)):
+            raise Exception("Step delay is too short: ", step_delay)
 
         for i in range(step_count):
-            self.pi.gpio_trigger(self.step, 1, step_delay)
+            self.pi.write(self.step, 1)
             time.sleep(step_delay)
+            self.pi.write(self.step, 0)
+            time.sleep(step_delay)  
+
+    def step_count_from_distance(self, distance):
+        full_revolution = self.TEETH_PER_REVOLUTION * self.TOOTH_PITCH
+        return int(distance / full_revolution * self.STEPS_PER_REVOLUTION)
+
+    def step_delay_from_speed(self, speed):
+        full_revolution = self.TEETH_PER_REVOLUTION * self.TOOTH_PITCH
+        steps_per_second = (speed / full_revolution) * self.STEPS_PER_REVOLUTION
+        step_delay = (1.0 / steps_per_second) # Whilst the delay should be calculated in millis, the function works in seconds
+        return step_delay
+
--- driver-files/test_laser_definition.py
+++ driver-files/test_laser_definition.py
@@ -1,10 +1,17 @@
 import pytest
 from laser_definition import Motor
 import pigpio
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
 
 class MockPi(pigpio.pi):
     def __init__(self):
         super().__init__()
+
+    def write(self, gpio, value):
+        print("write", gpio, value)
+        pass
 
     def set_mode(self, gpio, mode):
         print("set_mode", gpio, mode)
@@ -27,16 +34,27 @@ def test_motor_init():
 def test_motor_move():
     """Test motor move method"""
     motor = Motor(1, 2, 3, 4, 5, pi)
-    motor.move(10, 10)  # Basic smoke test - should not raise exception
+    motor.move(0.5, 20)  # Basic smoke test - should not raise exception
 
 def test_motor_speed_too_slow():
     """Test that motor raises an exception if the speed is too slow"""
     motor = Motor(1, 2, 3, 4, 5, pi)
     with pytest.raises(Exception):
-        motor.move(10, 0.000001)
+        motor.move(1, 1)
 
 def test_motor_speed_too_fast():
     """Test that motor raises an exception if the speed is too fast"""
     motor = Motor(1, 2, 3, 4, 5, pi)
     with pytest.raises(Exception):
-        motor.move(10, 1000000)
+        motor.move(1, 1000)
+
+def test_step_count_from_distance():
+    """Test that step_count_from_distance returns the correct number of steps"""
+    motor = Motor(1, 2, 3, 4, 5, pi)
+    assert motor.step_count_from_distance(10) == 50
+    assert motor.step_count_from_distance(0.5) == 2
+
+def test_step_delay_from_speed():
+    """Test that step_delay_from_speed returns the correct delay"""
+    motor = Motor(1, 2, 3, 4, 5, pi)
+    assert motor.step_delay_from_speed(20) == 10 / 1000.0
